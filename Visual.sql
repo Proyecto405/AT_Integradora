927579e372fc772a73438b45901f022ab1169f74
Proyecto405 <129135311+Proyecto405@users.noreply.github.com>
2023-03-31 19:42:51 -0600

    Add files via upload

---
+++ Visual.sql
@@ -0,0 +1,6 @@
+INSERT INTO proyecto405.tusuario(Id_Tusuario,Nombre_t,Descripcion)
+VALUES('1', 'Victor','Usuario Final'),
+('2', 'Luis','Usuario especializado'),
+('3', 'Francisco','Usuario Tecnico'),
+('4','Paola','Usuario Final'),
+('5', 'Alondra','Usuario Final');
