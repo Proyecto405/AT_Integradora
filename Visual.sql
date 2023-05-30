5a37e72237cab04a22d2229237789a04bfffba4a
Proyecto405 <129135311+Proyecto405@users.noreply.github.com>
2023-05-29 20:43:35 -0600

    Delete Visual.sql

--- Visual.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO proyecto405.tusuario(Id_Tusuario,Nombre_t,Descripcion)
-VALUES('1', 'Victor','Usuario Final'),
-('2', 'Luis','Usuario especializado'),
-('3', 'Francisco','Usuario Tecnico'),
-('4','Paola','Usuario Final'),
-('5', 'Alondra','Usuario Final');
